f967fd494017a195dccd91b000c45529bd8a8925
robertoDGS12 <itic18.roberto.guillen@iunis.edu.mx>
2022-11-30 08:56:46 -0600

    añadiendo codigo en las clases main

--- app/src/main/java/com/iunis/proyectos/iunisproject/SignInActivity.kt
+++ app/src/main/java/com/iunis/proyectos/iunisproject/SignInActivity.kt
@@ -6,12 +6,30 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.iunis.proyectos.iunisproject.databinding.ActivitySignInBinding
+import com.iunis.proyectos.iunisproject.databinding.ActivitySignUpBinding
 
 class SignInActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.textView.setOnClickListener{
+            val intent =Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+
+        }
+        binding.button.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+        }
+
+
 
         setContentView(R.layout.activity_sign_in)
     }
